32d9f3b26f8e91bceb16159815f648745ca8ef7e
funnybr0ther <guillaumevdrest@gmail.com>
2022-04-03 19:43:12 +0200

    Ça avance

--- src/dsp/oscillator.rs
+++ src/dsp/oscillator.rs
@@ -1,28 +1,93 @@
+use rand::distributions::{Distribution, Normal};
+use std::f32::consts::*;
+
 /// Base oscillator types (including `Noise`) or custom oscillator
 pub enum OscillatorType {
     /// Sine function
-    Sine,
+    Sine(SinOscillator),
     /// Square function of any duty cycle (ratio of positive signal over a whole cycle)
-    Square{duty_cycle: f32},
+    Square { duty_cycle: f32 },
     /// Impulse function with any duty cycle (ratio of on signal over a whole cycle)
-    Impulse{duty_cycle: f32},
+    Impulse { duty_cycle: f32 },
     /// Triangle wave with any duty cycle
-    Triangle{duty_cycle: f32},
+    Triangle { duty_cycle: f32 },
     /// Noise function TODO: Filter after generating white noise, or generate any color of noise
     Noise,
     /// Other (custom) waveform. See struct [CustomWaveForm]
-    Other{defining_function: CustomWaveForm }
+    Other { defining_function: CustomWaveForm },
 }
-
+/// Represents a custom waveform for a custom oscillator. The function can take [parameters_count]
+/// parameters and can also be given a name.
 pub struct CustomWaveForm {
-    function: fn(Vec<f32>) -> f32,
+    function: fn(&[f32], f32) -> f32,
     name: String,
-    parameters_count: u32
+    parameters_count: u32,
+    parameters: OscillatorParameters,
 }
 
-struct Oscillator{
+pub struct OscillatorParameters {
     freq: f32,
     amplitude: f32,
     phase: f32,
-    oscillator_type: OscillatorType
+}
+
+pub struct SinOscillator {
+    parameters: OscillatorParameters,
+}
+
+// pub fn eval_oscillator_time(oscillator: &Oscillator, t: f32) -> f32{
+//     match oscillator.oscillator_type{
+//         OscillatorType::Sine(osc) => {
+//             osc.osc(t,oscillator.phase,oscillator.freq,oscillator.amplitude)
+//         },
+//         OscillatorType::Noise(osc) => {
+//             osc.osc(t)
+//         }
+//     }
+// }
+
+// trait OscTrait {
+//     type Parameter: Copy;
+//
+//     fn osc(&self, p: Self::Parameter) -> f32;
+// }
+//
+// impl OscTrait for Oscillator<Sine, ()> {
+//     fn osc(&self) -> f32 {
+//         // ...
+//     }
+// }
+//
+// impl OscTrait for Oscillator<Square, ()>
+
+pub fn osc_sin(t: f32, phase: f32, freq: f32, amplitude: f32) -> f32 {
+    (freq * 2.0 * PI * t + phase * 2.0 * PI).sin() * amplitude
+}
+
+pub fn osc_square(t: f32, phase: f32, freq: f32, amplitude: f32, duty_cycle: f32) -> f32 {
+    let period = 1.0 / freq;
+    let time_relative = ((t + phase).rem_euclid(period)) / period;
+    if time_relative < duty_cycle {
+        amplitude
+    } else {
+        -amplitude
+    }
+}
+
+pub fn osc_noise(amplitude: f32) -> f32 {
+    let normal = Normal::new(0.0, 1.0);
+    let v = normal.sample(&mut rand::thread_rng());
+    v as f32 * amplitude
+}
+
+pub fn osc_triangle(t: f32, phase: f32, freq: f32, amplitude: f32, duty_cycle: f32) -> f32 {
+    let period = 1.0 / freq;
+    let time_relative = ((t + phase).rem_euclid(period)) / period;
+    if (duty_cycle == 0.0) | (duty_cycle == 1.0) {
+        ((duty_cycle - 0.5) * 2.0) * (-amplitude + amplitude * 2.0 * time_relative)
+    } else if time_relative < duty_cycle {
+        -amplitude + amplitude * 2.0 * (time_relative / duty_cycle)
+    } else {
+        amplitude - amplitude * 2.0 * ((1.0 - time_relative) / (1.0 - duty_cycle))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-mod math;
 mod dsp;
+mod math;
 
-use std::any::Any;
+use rodio::{Decoder, OutputStream, Sink, Source};
 use std::f32::consts::PI;
 use std::fs::File;
 use std::io::BufReader;
@@ -9,75 +9,80 @@ use std::ops::Rem;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use rodio::{Decoder, OutputStream, Sink, Source};
 
 #[derive(Clone, Debug)]
 pub struct AnyWave {
-        freq: f32,
-        num_sample: usize,
-        sampling_rate: u32
+    freq: f32,
+    num_sample: usize,
+    sampling_rate: u32,
+    master: f32,
+    instant_phase: f32,
 }
 
 impl AnyWave {
-        /// The frequency of the sine.
-        #[inline]
-        pub fn new(freq: f32) -> AnyWave {
-                AnyWave {
-                        freq: freq,
-                        num_sample: 0,
-                        sampling_rate: 44100
-                }
+    #[inline]
+    pub fn new(freq: f32, master: f32) -> AnyWave {
+        AnyWave {
+            freq,
+            num_sample: 0,
+            sampling_rate: 44100,
+            master,
+            instant_phase: 0.0,
         }
+    }
 }
 
 impl Iterator for AnyWave {
-        type Item = f32;
+    type Item = f32;
 
-        #[inline]
-        fn next(&mut self) -> Option<f32> {
-                let k:f32 = 150.0;
-                let fm_hz = 2.0;
-                self.num_sample = self.num_sample.wrapping_add(1);
-                let carrier = 2.0 * PI * self.num_sample as f32 * self.freq / self.sampling_rate as f32;
-                let modulator = k * (2.0 * PI * self.num_sample as f32 * fm_hz / self.sampling_rate as f32).sin();
-                let value = (carrier + modulator).cos()*0.3;
-                Some(value)
-        }
+    #[inline]
+    fn next(&mut self) -> Option<f32> {
+        let k: f32 = 200.0;
+        self.num_sample = self.num_sample.wrapping_add(1);
+        let t = self.num_sample as f32 / self.sampling_rate as f32;
+        let modulator = k * dsp::oscillator::osc_triangle(t, 0.0, 2.0, 1.0, 0.5);
+        self.instant_phase += 2.0 * PI * self.freq / self.sampling_rate as f32
+            + 2.0 * modulator / self.sampling_rate as f32;
+        let value = (self.instant_phase).sin();
+        println!("{}", value);
+        Some(value * self.master)
+    }
 }
 
 impl Source for AnyWave {
-        #[inline]
-        fn current_frame_len(&self) -> Option<usize> {
-                None
-        }
+    #[inline]
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
 
-        #[inline]
-        fn channels(&self) -> u16 {
-                1
-        }
+    #[inline]
 
-        #[inline]
-        fn sample_rate(&self) -> u32 {
-                self.sampling_rate
-        }
+    fn channels(&self) -> u16 {
+        1
+    }
 
-        #[inline]
-        fn total_duration(&self) -> Option<Duration> {
-                None
-        }
+    #[inline]
+    fn sample_rate(&self) -> u32 {
+        self.sampling_rate
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
 }
 
 fn main() {
     // Get a output stream handle to the default physical sound device
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     // Load a sound from a file, using a path relative to Cargo.toml
     //  let file = BufReader::new(File::open("example.ogg").unwrap());
     // Decode that sound file into a source
     //  let source = Decoder::new(file).unwrap();
-        let source = AnyWave::new(1000.0);
+    let source = AnyWave::new(1000.0, 0.5);
     // Play the sound directly on the device
-        let sink = Sink::try_new(&stream_handle).unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
 
-        sink.append(source);
-        sink.sleep_until_end();
+    sink.append(source);
+    sink.sleep_until_end();
 }
